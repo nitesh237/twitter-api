842a3b4819be425fa440beff6b64016606d16546
Nitesh Gupta <n.guptasbp@gmail.com>
2018-11-17 12:26:04 +0530

    ERROr

--- index.js
+++ index.js
@@ -2,62 +2,62 @@ const express = require("express"),
 mongoose = require("mongoose"),
 app =  express(),
 config = require('./config'),
-session = require('express-session'),
-uuid = require('uuid/v4'),
-mongoStore = require('connect-mongo')(session),
-bodyParser = require('body-parser'),
-bcrypt = require('bcryptjs'),
-passport = require('passport'),
-LocalStrategy = require('passport-local').Strategy,
+// session = require('express-session'),
+// uuid = require('uuid/v4'),
+//mongoStore = require('connect-mongo')(session),
+//bodyParser = require('body-parser'),
+//bcrypt = require('bcryptjs'),
+// passport = require('passport'),
+// LocalStrategy = require('passport-local').Strategy,
 User = require('./user/User'),
 Tweet = require('./tweet/Tweet')
 
 
 global.__root = __dirname + '/'
-mongoose.connect("mongodb://localhost:27017/twitter-api-app", {useNewUrlParser: true})
+
 //mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
-passport.use(new LocalStrategy(
-  (username, password, done) => {
-    console.log('Inside local strategy callback')
-    User.findOne({ username: username }, (err, user) => {
-    	if (err) { return done(err) }
-    	if (!user) { return done(null, false) }
-    	var passwordIsValid = bcrypt.compareSync(password, user.password)
-    	if (!passwordIsValid) { return done(null, false) }
-    	console.log('Local strategy returned true')
-    	return done(null, user)
-    })
-   }
-))
+// passport.use(new LocalStrategy(
+//   (username, password, done) => {
+//     console.log('Inside local strategy callback')
+//     User.findOne({ username: username }, (err, user) => {
+//     	if (err) { return done(err) }
+//     	if (!user) { return done(null, false) }
+//     	var passwordIsValid = bcrypt.compareSync(password, user.password)
+//     	if (!passwordIsValid) { return done(null, false) }
+//     	console.log('Local strategy returned true')
+//     	return done(null, user)
+//     })
+//    }
+// ))
 
-passport.serializeUser((user, done) => {
-  console.log('Inside serializeUser callback. User username is save to the session file store here')
-  done(null, user.username);
-})
+// passport.serializeUser((user, done) => {
+//   console.log('Inside serializeUser callback. User username is save to the session file store here')
+//   done(null, user.username);
+// })
 
-passport.deserializeUser((username, done) => {
-  console.log('Inside deserializeUser callback')
-  console.log(`The user name passport saved in the session file store is: ${username}`)
-  User.findOne({username: username}, (err, user) => {
-  	if (err) { return done(err) }
-    	if (!user) { return done(null, false) }
-    	return done(null, user)
-  })
-})
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(session({
-  genid: (req) => {
-    console.log('Generating new session ID')
-    return uuid() 
-  },
-  store: new mongoStore({ mongooseConnection: mongoose.connection, autoRemove: 'interval', autoRemoveInterval: 120 }),
-  secret: 'supersecretkey',
-  resave: false,
-  saveUninitialized: true
-}))
-app.use(passport.initialize());
-app.use(passport.session());
+// passport.deserializeUser((username, done) => {
+//   console.log('Inside deserializeUser callback')
+//   console.log(`The user name passport saved in the session file store is: ${username}`)
+//   User.findOne({username: username}, (err, user) => {
+//   	if (err) { return done(err) }
+//     	if (!user) { return done(null, false) }
+//     	return done(null, user)
+//   })
+// })
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
+// app.use(session({
+//   genid: (req) => {
+//     console.log('Generating new session ID')
+//     return uuid() 
+//   },
+//   store: new mongoStore({ mongooseConnection: mongoose.connection, autoRemove: 'interval', autoRemoveInterval: 120 }),
+//   secret: 'supersecretkey',
+//   resave: false,
+//   saveUninitialized: true
+// }))
+// app.use(passport.initialize());
+// app.use(passport.session());
 app.get("/", (req, res) => {
 
 	res.send(`Hello`)
@@ -73,60 +73,60 @@ function requireLogin(req,res,next) {
 	}
 }
 
-app.post('/register', async (req, res) => {
+// app.post('/register', async (req, res) => {
 		
-    console.log(req.body)
-  	if(!req.body.username || !req.body.name || !req.body.password) {
-  		return res.send({message: "Missing Values"})
-  	}
+//     console.log(req.body)
+//   	if(!req.body.username || !req.body.name || !req.body.password) {
+//   		return res.send({message: "Missing Values"})
+//   	}
    	
-  	var user = await User.findOne({ username: req.body.username })
-  	//console.log(user)
-  	if(!user)
-  	{
-  		var hashedPassword = bcrypt.hashSync(req.body.password, 8)
-  		var user2 = await User.create(
-    	{
-    	  	name : req.body.name,
-    	    username : req.body.username,
-    		password : hashedPassword
-    	})
-  		if(user2 && user2.username) {
-  			res.send({message: "Registered Successfully"})
-  		} else {
-  			res.send({error: "Error at server side"})
-  		}	
-  	} else if(user.username){
-  		res.send({message: "User Already Exists!"})
-  	} else {
-  		res.send({error: "Error at server side"})
-  	}
-})
-app.post('/login', passport.authenticate('local'), (req, res) => {
-  	//console.log("Inside post Login Callback")
-  	//console.log(req.session.passport)
-  	//console.log(req.user)
-    res.send("LOGGED IN")
-})
-// app.get('/authrequired', (req, res) => {
-//   console.log('Inside GET /authrequired callback')
-//   console.log(`User authenticated? ${req.isAuthenticated()}`)
-//   if(req.isAuthenticated()) {
-//     res.send('you hit the authentication endpoint\n')
-//   } else {
-//     res.redirect('/')
-//   }
+//   	var user = await User.findOne({ username: req.body.username })
+//   	//console.log(user)
+//   	if(!user)
+//   	{
+//   		var hashedPassword = bcrypt.hashSync(req.body.password, 8)
+//   		var user2 = await User.create(
+//     	{
+//     	  	name : req.body.name,
+//     	    username : req.body.username,
+//     		password : hashedPassword
+//     	})
+//   		if(user2 && user2.username) {
+//   			res.send({message: "Registered Successfully"})
+//   		} else {
+//   			res.send({error: "Error at server side"})
+//   		}	
+//   	} else if(user.username){
+//   		res.send({message: "User Already Exists!"})
+//   	} else {
+//   		res.send({error: "Error at server side"})
+//   	}
 // })
-//})
-app.get('/logout', requireLogin, (req, res) => {
-	req.session.destroy((err) => {
-		if(err) {
-			res.send(err)
-		} else {
-			res.send("Logout SuccFull")
-		}
-	})
-})
+// app.post('/login', passport.authenticate('local'), (req, res) => {
+//   	//console.log("Inside post Login Callback")
+//   	//console.log(req.session.passport)
+//   	//console.log(req.user)
+//     res.send("LOGGED IN")
+// })
+// // app.get('/authrequired', (req, res) => {
+// //   console.log('Inside GET /authrequired callback')
+// //   console.log(`User authenticated? ${req.isAuthenticated()}`)
+// //   if(req.isAuthenticated()) {
+// //     res.send('you hit the authentication endpoint\n')
+// //   } else {
+// //     res.redirect('/')
+// //   }
+// // })
+// //})
+// app.get('/logout', requireLogin, (req, res) => {
+// 	req.session.destroy((err) => {
+// 		if(err) {
+// 			res.send(err)
+// 		} else {
+// 			res.send("Logout SuccFull")
+// 		}
+// 	})
+// })
 
 app.post('/follow', requireLogin, async(req, res) => {
 
@@ -235,15 +235,17 @@ app.delete('/tweet', requireLogin, (req, res) => {
 app.get("/api/status", (req, res) => {
 	res.send('------SERVER RUNNING----')
 })
-// const AuthController = require(__root + 'auth/AuthController')
-// app.use('/api/auth', AuthController)
+
 
 // const UserController = require(__root + 'user/UserController')
 // app.use('/api/user', UserController)
-
+var db = mongoose.connect("mongodb://localhost:27017/twitter-api-app", {useNewUrlParser: true})
 let port = config.port
 app.listen(3000, () => {
   console.log(`Starting on port ${3000}`)
 })
 
+//console.log(mongoose)
+const AuthController = require(__root + 'auth/AuthController')
+app.use('/api/auth', AuthController)
 module.exports = app
